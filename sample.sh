f605c4185fa30bc950eb609b3c20dd498f15ff61
tajuddin <tajuddin.n@sonata-software.com>
2024-03-13 12:41:32 +0530

    sample.sh file updated

--- sample.sh
+++ sample.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "This is my sample shell script"
+echo "This is my sample existing shell script"
 echo "My shell script name is $0"
 echo "My hostname is: $(hostname)"
 echo "Print disk space"
 df -h
+echo "This is the new line"
